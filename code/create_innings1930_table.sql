7ec00ecd6f3ee8c3938be5d64e6042dd7ba8a597
Sean Carver <seancarverphd@gmail.com>
2019-04-11 18:44:23 -0400

    Added innings1930 creator.

---
+++ code/create_innings1930_table.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS retrosheet.innings1930;
+CREATE TABLE retrosheet.innings1930 (
+  game_id VARCHAR(12) NOT NULL,
+  inning TINYINT NOT NULL,                    -- 1 or higher
+  batting_team BOOLEAN NOT NULL,              -- 0=visitor, 1=home
+  home_team VARCHAR(3) DEFAULT NULL,
+  visiting_team VARCHAR(3) DEFAULT NULL,
+  at_bat VARCHAR(3) DEFAULT NULL,
+  at_field VARCHAR(3) DEFAULT NULL,
+  year INT(4) DEFAULT NULL,
+  date DATE  DEFAULT NULL,
+  double_header_game INT(1) DEFAULT NULL,
+  finalxxx BOOLEAN DEFAULT NULL,
+  all_batter BOOLEAN DEFAULT NULL,
+  sequence VARCHAR(250) DEFAULT NULL,
+  u_sequence DOUBLE DEFAULT NULL,
+  PRIMARY KEY (game_id,inning,batting_team)
+) ENGINE=MyISAM;
